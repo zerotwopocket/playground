bbccb2de7f56427972cfa2da47fceb3419230697
02pocket <02pocket@dummyemail.net>
2022-09-25 22:55:26 +0800

    cleanup: remove unused fields

--- springboot/security/src/main/java/com/zerotwopocket/security/model/Account.java
+++ springboot/security/src/main/java/com/zerotwopocket/security/model/Account.java
@@ -1,15 +1,11 @@
 package com.zerotwopocket.security.model;
 
-import java.util.Set;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
 public class Account{
-  private Long id;
   private String username;
   private String password;
-  private Set<String> roles;
-  private Set<String> permissions;
 }
