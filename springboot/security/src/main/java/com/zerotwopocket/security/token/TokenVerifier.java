834b6bfe090e1d6852a95f1c23c3c63adf9c6e62
02pocket <02pocket@dummyemail.net>
2022-09-26 21:02:02 +0800

    cleanup: organize imports

--- springboot/security/src/main/java/com/zerotwopocket/security/token/TokenVerifier.java
+++ springboot/security/src/main/java/com/zerotwopocket/security/token/TokenVerifier.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.zerotwopocket.security.service.AuthenticationConfigConstants;
-import org.springframework.stereotype.Component;
 
 // @Component
 public class TokenVerifier {
